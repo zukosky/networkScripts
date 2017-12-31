7f3b5cfc49bf7a3e4a2c7abf730b32e685ca1de8
zukosky <zukosky@sbcglobal.net>
2017-12-31 10:46:27 -0600

    First release of probe sniffer

--- probeReqSniffer.py
+++ probeReqSniffer.py
@@ -11,16 +11,36 @@ import datetime
 mac_list = []
 mac_count = []
 TAB_1 = "\t"
-def PacketHandler(pkt) :
-print("num"+TAB_1+"Time"+TAB_1+"MAC"+TAB_1+"ESSID")
-  if pkt.haslayer(Dot11) :
-		if pkt.type == 0 and pkt.subtype == 4 :
-			#pkt.summary()
-			#Print if the combined MAC+AP
-			if len(pkt.info) >0 and pkt.addr2 not in mac_list :
-				mac_list.append(pkt.addr2)
-				print "%s\t%s\t%s\t%s" %(str(len(mac_list)),str(datetime.datetime.now().time())[0:8],pkt.addr2, pkt.info)
-#				print str(len(mac_list)) + "  "+str(datetime.datetime.now().time())[0:8] + " DEVICE MAC: %s PROBING SSID: %s " %(pkt.addr2, pkt.info)
-#				print str(len(mac_list)) + "  "+str(datetime.datetime.now().time())[0:8] + " DEVICE MAC: %s PROBING SSID: %s " %(pkt.addr2, pkt.info)
+formatString ="{: <4} {: <10} {: <20} {: <20} {: <40}"
+#formatString ="{: <4} {: <10} {: <20} {: <40}"
+#Read the OUI Reference FIle
+fName = "oui-clean.txt"
+fileHandle = open(fName, 'r')
+oui =[]
+manu = []
+for line in fileHandle:
+    fields = line.split('|')  # Only reads one line at a time
+    oui.append(fields[0])
+    manu.append(fields[1].rstrip())  #.rstrip() removes the trailing \n
+fileHandle.close()
+ouiRef = dict(zip(oui,manu))
+#print(ouiRef.keys())
+#print(ouiRef.values())
+def uniquePacketHandler(pkt) :
+    if pkt.haslayer(Dot11) :
+        if pkt.type == 0 and pkt.subtype == 4 :
+		    #pkt.summary()
+		    #Print if the combined MAC+AP
+            if len(pkt.info) >0 and pkt.addr2 not in mac_list :
+                mac_list.append(pkt.addr2)
+                thisMac = pkt.addr2[:8].replace(':','-')
+                thisMac = thisMac.upper()
+                thisOui=ouiRef.get(thisMac,"")
+                #print formatString.format(str(len(mac_list)), str(datetime.datetime.now().time())[0:8],pkt.addr2, pkt.info)
+                print formatString.format(str(len(mac_list)), str(datetime.datetime.now().time())[0:8],
+                                          pkt.addr2, pkt.info, thisOui)
+
+
 #Scapy by default stores all packets.  Need store=0.
-sniff(iface="wlan0", prn = PacketHandler,store=0)
+print formatString.format ("num","Time","MAC","ESSID","Manufacturer")
+sniff(iface="wlan0", prn = uniquePacketHandler, store=0)
