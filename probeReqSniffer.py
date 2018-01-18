9b5a81629552812b7d39f363ce8634e4793dab1e
zukosky <zukosky@sbcglobal.net>
2018-01-17 21:53:34 -0600

    Adding MAC type function

--- probeReqSniffer.py
+++ probeReqSniffer.py
@@ -9,13 +9,44 @@ import sys
 # list all of the unique MAC address - Probe combinations
 #
 #################################################################
+#################################################################
+# Function for deciding if a MAC address is random/administratively assigned
+#################################################################
+def getMACAddressType(MACAddress):
+    # Administratively assigned addresses have bit 1 = 1
+    binVal = int(MACAddress[:2], 16)
+    if((binVal & 2)>0):
+        MACType = "Assigned"
+    else:
+        MACType = "Factory"
+    return(MACType)
+#################################################################
+# Function for reading the list of local MAC address (my own devices)
+#################################################################
+def getLocalMACs():
+    oui=[]
+    manu=[]
+    desc=[]
+    lmfname= "localMAC.txt"
+    fileHandle = open(lmfname, 'r')
+    for line in fileHandle:
+        fields = line.split('|')  # Only reads one line at a time
+        oui.append(fields[0])
+        manu.append(fields[1])
+        desc.append(fields[2].rstrip())
+    fileHandle.close()
+    #localMACs = {oui[i]:manu[i]:desc[i] for i in xrange(len(oui))}
+    #localMACs = dict(zip(oui,zip(manu,desc)))
+    localMACs = dict((z[0],list(z[1:])) for z in zip(oui,manu,desc))
+#    localMACs = {z[0]:list(z[1:])) for z in zip(oui,manu,desc)}
+    return localMACs
+
 mac_list = []
 mac_count = []
 TAB_1 = "\t"
 DOT11_MANAGEMENT_FRAME = 0
 DOT11_PROBE_REQUEST = 4
-formatString ="{: <4} {: <10} {: <10} {: <20} {: <20} {: <40}"
-#formatString ="{: <4} {: <10} {: <20} {: <40}"
+formatString ="{: <4} {: <10} {: <10} {: <20} {: <20} {: <40} {: <10}"
 #Read the OUI Reference FIle
 fName = "oui-clean.txt"
 fileHandle = open(fName, 'r')
@@ -29,6 +60,9 @@ for line in fileHandle:
     manu.append(fields[1].rstrip())  #.rstrip() removes the trailing \n
 fileHandle.close()
 ouiRef = dict(zip(oui,manu))
+
+localMACs = getLocalMACs()
+print(localMACs)
 #print(ouiRef.keys())
 #print(ouiRef.values())
 def uniquePacketHandler(pkt) :
@@ -49,7 +83,7 @@ def uniquePacketHandler(pkt) :
                     #ls(pkt)
                     #print formatString.format(str(len(mac_list)), str(datetime.datetime.now().time())[0:8],pkt.addr2, pkt.info)
                     print formatString.format(str(len(mac_list)), str(datetime.datetime.now().time())[0:8],
-                                              rssi, pkt.addr2, pkt.info, thisOui)
+                                              rssi, pkt.addr2, pkt.info, thisOui,getMACAddressType(pkt.addr2))
                     fileOutHandle.write(formatString.format(str(len(mac_list)), str(datetime.datetime.now().time())[0:8],
                                               rssi, pkt.addr2, pkt.info, thisOui))
     except KeyboardInterrupt:
@@ -57,7 +91,7 @@ def uniquePacketHandler(pkt) :
         sys.exit()
 
 #Scapy by default stores all packets.  Need store=0.
-print formatString.format ("num","Time","Power","MAC","ESSID","Manufacturer")
+print formatString.format ("num","Time","Power","MAC","ESSID","Manufacturer","MACType")
 while (True):
     try:
         sniff(iface="wlan0", prn = uniquePacketHandler, store=0)
